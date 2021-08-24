ccecb47270f8f4553af20758dfadb0ea1bd14a2f
yukisa2010 <yukisa2010@gmail.com>
2021-08-24 20:23:02 +0900

    Add customer-data component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-const app = new Vue({
-    el: '#app',
+Vue.component('search-field', {
+    props: ["organizations"],
+    data() {
+        return {
+            inputParams: {
+                name: "",
+                gender: "",
+                organizationId: ""
+            }
+        }
+    },
     template: `
-<div>
     <div id="search-field">
         <span>氏名</span>
         <input type="text" name="name" v-model="inputParams.name">
@@ -23,8 +31,21 @@ const app = new Vue({
                 {{ organization.name }}
             </option>
         </select>
-        <button @click="queryData">検索</button>
+        <p>
+            <button @click="queryData">検索</button>
+        </p>
     </div>
+    `,
+    methods: {
+        queryData() {
+            this.$emit('query', this.inputParams)
+        }
+    }
+})
+
+Vue.component('customers-table', {
+    props: ["rawCustomers", "queryCustomers", "organizations"],
+    template: `
     <div id="data-field">
         <table :style="tableStyles">
             <tr :styles="tableStyles">
@@ -34,72 +55,101 @@ const app = new Vue({
                 <th>生年月日</th>
                 <th>所属会社</th>
             </tr>
-            <tr v-for="customer in queryCustomers" :styles="tableStyles">
-                <td>{{ customer.id }}</td>
-                <td>{{ customer.name }}</td>
-                <td>{{ customer.gender }}</td>
-                <td>{{ customer.birthday }}</td>
-                <td>{{ getOrganizationName(customer.organizationId) }}</td>
-            </tr>
+            <customer-data 
+                v-for="customer in queryCustomers" 
+                :customer="customer"
+                :organizations = "organizations"
+                :styles="tableStyles"
+            >
+            </customer-data>
         </table>
     </div>
-</div>
+
+    `,
+    computed: {
+        tableStyles() {
+            return {
+                border: 'solid 1px black',
+                borderCollapse: 'collapse',
+                padding: '10px'
+            }
+        }
+    }
+})
+
+Vue.component('customer-data', {
+    props: ["customer", "organizations"],
+    template: `
+    <tr>
+        <td>{{ customer.id }}</td>
+        <td>{{ customer.name }}</td>
+        <td>{{ customer.gender }}</td>
+        <td>{{ customer.birthday }}</td>
+        <td>{{ getOrganizationName(customer) }}</td>
+    </tr>
+    `,
+    methods: {
+        getOrganizationName(customer) {
+            const targetOrganization = this.organizations.find(organization => {
+                return organization.id === customer.id
+            });
+            return targetOrganization.name
+        }
+    }
+})
+
+const app = new Vue({
+    el: '#app',
+    template: `
+    <div>
+        <search-field 
+            :organizations="organizations" 
+            @query="queryData"
+        ></search-field>
+        <customers-table
+            :rawCustomers = rawCustomers
+            :queryCustomers = queryCustomers
+            :organizations = organizations
+        ></customers-table>
+    </div>
     `,
     data() {
         return {
-            inputParams: {
-                name: "",
-                gender: "",
-                organizationId: ""
-            },
             rawCustomers: [],
             queryCustomers: [],
             organizations: []
         }
     },
     methods: {
-        queryData() {
-            const reg = new RegExp(this.inputParams.name)
-            const gender = this.inputParams.gender
-            const organizationId = this.inputParams.organizationId
-            const queryCustomers = this.rawCustomers.filter(customer => {
+        queryData(inputParams) {
+            const reg = new RegExp(inputParams.name)
+            const gender = inputParams.gender
+            const organizationId = inputParams.organizationId
+            this.rawCustomers = this.rawCustomers.filter(customer => {
                 return (
                     reg.test(customer.name)
                     && (gender === "" ? true : customer.gender === gender )
                     && (organizationId === "" ? true: customer.organizationId === organizationId)
                 )
             })
-
-            this.queryCustomers = queryCustomers
-        },
-        getOrganizationName(id) {
-            const targetOrganization = this.organizations.filter(organization => {
-                return organization.id === id
-            });
-    
-            return targetOrganization[0]["name"]
-        },
-    },
-    computed: {
-        tableStyles() {
-            return {
-                border: 'solid 1px black',
-                borderCollapse: 'collapse'
-            }
         }
     },
     beforeCreate() {
         axios
             .get('./customers.json')
             .then(res => {
-                this.rawCustomers = res.data
+                this.rawCustomers =  res.data
                 this.queryCustomers = this.rawCustomers
+                return res
             })
         axios
             .get('./organizations.json')
             .then(res => {
                 this.organizations = res.data
-            })
+            })                
+    },
+    mounted() {
+        this.queryData(this.inputParams)
 
     }
 })
